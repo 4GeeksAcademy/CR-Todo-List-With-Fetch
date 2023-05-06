b7fa11a584e783d9e34acc5d0595b49afa0b88a0
Cat <126123826+CRob001@users.noreply.github.com>
2023-05-06 12:51:27 +0000

    First Start Todo List with Fetch Project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const element = <FontAwesomeIcon icon={faTrashAlt} />;
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="container">
+      <h1>Todos</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="What do you need to do?"
+          />
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <span style={{ float: "right" }}>
+              <FontAwesomeIcon
+                icon={faTrashAlt}
+                onClick={() =>
+                  setTodos(
+                    todos.filter((t, currentIndex) => index !== currentIndex)
+                  )
+                }
+              />
+            </span>
+          </li>
+        ))}
+      </ul>
+      <div>{todos.length} tasks</div>
+    </div>
+  );
 };
 
 export default Home;
